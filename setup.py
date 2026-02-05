7a8600f1b6e44693d0ee3576dd936922c728db64
ArProvat <provatar0@gamil.com>
2026-02-05 10:54:03 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Actyv_AI"
+AUTHOR_USER_NAME = "ArProvat"
+SRC_REPO = "app" 
+AUTHOR_EMAIL = "provatar0@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="GenAI Project structure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "app"}, 
+    packages=setuptools.find_packages(where="app")
+)
